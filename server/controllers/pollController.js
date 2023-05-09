c63d2830460696128fb3ac77c44eb3c0007fde46
“Aissaiyah <aissaiyah.bennett@bronxsoftware.org>
2023-05-09 13:20:30 -0400

    extended application of school refrence

--- server/controllers/pollController.js
+++ server/controllers/pollController.js
@@ -30,21 +30,21 @@ const jobList = (req, res) => {
 /***************
  * The poll function receives the user input and stores it the database
  **************/
-const poll = (req, res, next) => {
-  const { email, JobTitle, CreatorEmail } = req.body;
-
+const poll = (req, res) => {
+  const { email, SchoolID, JobTitle, CreatorEmail } = req.body;
+ 
   bcrypt
     .hash(email.split("@")[0], process.env.SALT)
     .then((hashedUser) => {
       return new Promise((resolve, reject) => {
         dbUserConn.query(
           `
-                        Replace Student_Job_Interest 
-                        (User, School, JobTitle, CreatorEmail)
+                        Replace Student_Job_Interest
+                        (User, School, SchoolID, JobTitle, CreatorEmail)
                         values
-                        (?,?,?,?);
+                        (?,?,?,?,?);
                 `,
-          [hashedUser, email.split("@")[1], JobTitle, CreatorEmail],
+          [hashedUser, email.split("@")[1], SchoolID, JobTitle, CreatorEmail],
           (err, results) => {
             if (err) {
               reject(err);
@@ -60,36 +60,70 @@ const poll = (req, res, next) => {
       console.log(err)
       res.sendStatus(500)
     });
-};
+ };
+ 
+ 
+ const results = (req, res) => {
+  const { SchoolID } = req.body
+ 
+  if(SchoolID === null) {
+      return new Promise((resolve, reject) => {
+          dbUserConn.query(
+            `
+            Select B.SchoolName, count(*) as studentCount from Student_Job_Interest A
+            join NYC_High_Schools B on A.SchoolID = B.SchoolID
+            where A.SchoolID is not null
+            group by B.SchoolName
+            order by studentCount desc;
+              `,
+            (err, results) => {
+              if (err) {
+                reject(err);
+              }
+              resolve(results);
+            }
+          );
+        })   
+        .then(results => {
+          res.json(results)
+        })
+        .catch(err => {
+          console.log(err)
+          res.sendStatus(500)
+        });
+  }else{
+      return new Promise((resolve, reject) => {
+          dbUserConn.query(
+            `
+            Select JobTitle, count(*) as studentCount from Student_Job_Interest
+            where SchoolID = ?
+            group by JobTitle
+            order by studentCount desc;
+              `,[SchoolID],
+            (err, results) => {
+              if (err) {
+                reject(err);
+              }
+              resolve(results);
+            }
+          );
+        })   
+        .then(results => {
+          res.json(results)
+        })
+        .catch(err => {
+          console.log(err)
+          res.sendStatus(500)
+        });
+  }
+ 
+ };
+ 
+  
+ 
 
-/**************
- * The results function returns the poll results to the browser
- **************/
-const results = (req, res, next) => {
-  return new Promise((resolve, reject) => {
-    dbUserConn.query(
-      `
-      select JobTitle, count(*) as studentCount from Student_Job_Interest
-      where School = 'bronxsoftware.org'
-      group by JobTitle
-      order by studentCount desc;
-        `,
-      (err, results) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(results);
-      }
-    );
-  })    
-  .then(results => {
-    res.json(results)
-  }) 
-  .catch(err => {
-    console.log(err)
-    res.sendStatus(500)
-  });
-};
+
+
 
 // make the above code accessible for other code modules
 module.exports = { jobList, poll, results };
